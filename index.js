119cbaa998d56f552ab951f5438b7dd1777ea676
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2019-01-29 03:08:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function dwarfRollCall(dwarves) {
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+function dwarfRollCall(dwarves){
+  var newDwarves = "";
+  for (var i = 0; i < dwarves.length; i++){
+  if(i === dwarves.length){
+    newDwarves += (i+1 + ". " + dwarves[i]);
+  }else {
+    newDwarves += (i+1 + ". " + dwarves[i] + " ");
+  }
+}
+return newDwarves;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var planteerCalls = ['APPLE!', 'BANANA!', 'ORANGE!']
+  return planteerCalls
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(words){
+  for (var i = 1; i < words.length; i++) {
+    if(words.length>4){
+    return true
+  }
+}
+return false
 }
 
 function findTheCheese (foods) {
+  for (var i = 1; i <foods.length; i++){
+if (foods[i]=== "cheddar"
+|| foods[i] === "tomato soup"
+|| foods[i] === "crackers"
+){
+return foods[i]
+}
+}
+return "no cheese!"
 }
